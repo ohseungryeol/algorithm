0d8f98b6a03d3bde8e21c7eb14ded6e2fde06c6a
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2023-08-17 15:49:23 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.21 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,73 +1,148 @@
+import java.util.*;
 class Solution {
-    public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
+     static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
 
-        for(int t=0; t<places.length; t++) {
-            String[] map = places[t];
-            boolean flag = false;
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (map[i].charAt(j)=='P') {
-                        if (isAroundExistPerson(i, j, map)) {
-                            flag = true;
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        for (int z=0; z<5; z++) answer[z]=1;
+        char[][] map;
+        for (int i=0; i<places.length; i++){
+            boolean personsEmpty = true;
+            String[] tmp = places[i];
+            map = new char[5][5];
+            for (int j=0; j<5; j++){
+                for (int k=0; k<5; k++){
+                    map[j][k]=tmp[j].charAt(k);
+                }
+            }
+
+            for (int j=0; j<5; j++){
+                for (int k=0; k<5; k++){
+                    if(map[j][k]=='P'){
+                        personsEmpty=false;
+                        if(!OneDistance(j,k,map)){
+                            answer[i]=0;
                             break;
                         }
+                        if(!linearTwoDistance(j,k,map)){
+                            answer[i]=0;
+                            break;
+                        }
+                        if(!diagonalDistance(j,k,map)){
+                            answer[i]=0;
+                            break;
+                        }
+
                     }
                 }
-                if(flag){
-                    answer[t] = 0;
-                    break;
-                }
             }
-            if(!flag){
-                answer[t] = 1;
-            }
+            if(personsEmpty==true) answer[i]=1;
         }
 
+        System.out.println(Arrays.toString(answer));
         return answer;
     }
 
-     public static boolean isAroundExistPerson(int i, int j, String[] map){
-
-        // 상하좌우 확인
-        int[] mi = {0, 0, 1, -1};
-        int[] mj = {1, -1, 0, 0};
-        for (int d = 0; d < 4; d++) {
-            int ni = i + mi[d];
-            int nj = j + mj[d];
-            if(ni<0 || ni>=5 || nj<0 || nj>=5) continue;
-            if(map[ni].charAt(nj)=='P') return true;
+    public boolean OneDistance(int x,int y, char[][] map){
+        for (int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(isRange(nx,ny) && map[nx][ny]=='P')  return false;
         }
-        
-        // 상하좌우 맨하탄 거리 2 확인
-        int[] mi2 = {0, 0, 2, -2};
-        int[] mj2 = {2, -2, 0, 0};
-        for (int d = 0; d < 4; d++) {
-            int ni = i + mi2[d];
-            int nj = j + mj2[d];
-            if(ni<0 || ni>=5 || nj<0 || nj>=5) continue;
-            if(map[ni].charAt(nj)=='P') {
-                if(map[(i+ni)/2].charAt((j+nj)/2)!='X'){
-                    return true;
+
+        return true;
+    }
+
+    public boolean diagonalDistance(int x, int y, char[][] map){
+        int leftUpX = x-1;
+        int leftUpY = y-1;
+
+        if(isRange(leftUpX,leftUpY)){
+            if(map[leftUpX][leftUpY]=='P'){
+                if(map[x-1][y]!='X' || map[x][y-1]!='X'){
+                    return false;
                 }
             }
         }
 
-        // 대각선 확인
-        int[] mi3 = {1, 1, -1, -1};
-        int[] mj3 = {1, -1, 1, -1};
+        int rightUpX = x-1;
+        int rightUpY = y+1;
 
-        for (int d = 0; d < 4; d++) {
-            int ni = i + mi3[d];
-            int nj = j + mj3[d];
-            if(ni<0 || ni>=5 || nj<0 || nj>=5) continue;
-            if(map[ni].charAt(nj)=='P') {
-                if(!(map[i].charAt(nj)=='X' && map[ni].charAt(j)=='X')){
-                    return true;
+        if(isRange(rightUpX,rightUpY)){
+            if(map[rightUpX][rightUpY]=='P'){
+                if(map[x-1][y]!='X' && map[x][y+1]!='X') {
+                    return false;
                 }
             }
         }
 
+        int leftDownX = x+1;
+        int leftDownY = y-1;
+
+        if(isRange(leftDownX,leftDownY)){
+            if(map[leftDownX][leftDownY]=='P'){
+                if(map[x][y-1]!='X' || map[x+1][y]!='X') {
+                    return false;
+                }
+            }
+        }
+
+        int rightDownX = x+1;
+        int rightDownY = y+1;
+
+        if(isRange(rightDownX,rightDownY)){
+            if(map[rightDownX][rightDownY]=='P'){
+                if(map[x][y+1]!='X'|| map[x+1][y]!='X'){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean linearTwoDistance(int x, int y, char[][]map){
+        int upX = x-2;
+        int upY = y;
+
+        if(isRange(upX,upY)){
+            if(map[upX][upY]=='P'){
+                if(map[x-1][y]!='X') return false;
+            }
+        }
+
+        int rightX = x;
+        int rightY = y+2;
+
+        if (isRange(rightX,rightY)){
+            if(map[rightX][rightY]=='P'){
+                if(map[x][y+1]!='X') return false;
+            }
+        }
+        int downX = x+2;
+        int downY = y;
+
+        if(isRange(downX,downY)){
+            if(map[downX][downY]=='P'){
+                if(map[x+1][y]!='X') return false;
+            }
+        }
+
+        int leftX = x;
+        int leftY = y-2;
+
+        if(isRange(leftX,leftY)){
+            if(map[leftX][leftY]=='P'){
+                if(map[x][y-1]!='X') return false;
+            }
+        }
+
+        return true;
+    }
+     public boolean isRange(int x, int y){
+        if(x>=0 && x<5 && y>=0 && y<5) return true;
         return false;
     }
+
 }
