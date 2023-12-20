bd031548f1c89fed0d8a7854a5eb5b70aa564048
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2023-12-20 16:34:26 +0900

    [level 2] Title: 피로도, Time: 0.17 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,27 @@
 class Solution {
-     static int answer =0;
+     static int answer=0;
     static boolean[] visited;
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-
-        DFS(0,dungeons, k);
+        DFS(0, k, dungeons, 0);
         System.out.println(answer);
         return answer;
     }
 
-    public void DFS(int count,int[][] dungeons, int k){
-
-
-        answer = Math.max(answer, count);
+    public static void DFS(int L, int k, int[][] dungeons, int num){
+       // System.out.printf("DFS(%d %d %d)\n",L,k,num);
+        answer = Math.max(answer, num);
+        if(L==dungeons.length || k<=0){
+            return;
+        }
 
         for (int i=0; i< dungeons.length; i++){
-            int minPirodo = dungeons[i][0];
-            int finPirodo = dungeons[i][1];
-
-            if(!visited[i] && minPirodo<=k){
+            int[] info = dungeons[i];
+            int minPiro = info[0]; //최소 필요도
+            int minusPiro = info[1]; //소모 필요도
+            if(!visited[i] && k>=minPiro){
                 visited[i]=true;
-                DFS( count+1, dungeons, k-finPirodo);
+                DFS(L+1,k-minusPiro,dungeons,num+1);
                 visited[i]=false;
             }
         }
