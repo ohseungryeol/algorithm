4b22531771ef5e844d0bcc3880aa19c4414c86f8
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-05-24 16:18:58 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        // 10인분당 음료 1개 
+        
+        // n= 10 k=3 
+        // 12000*10+2000*k
+    
+        return ((12000*n) + (k*2000) - (n/10 *2000));
+    }
+}
