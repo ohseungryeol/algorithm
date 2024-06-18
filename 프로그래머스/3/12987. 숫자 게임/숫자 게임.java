894479697a357f3a2f564a3bafe53616647d510a
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-06-18 18:35:28 +0900

    [level 3] Title: 숫자 게임, Time: 108.29 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,35 +1,40 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-        
-        // 각 팀은 대결해서 승리한 쪽이 +1점 
-        // A팀의 순서를 보고 자신들의 승점을 가장 높이는 방법은 ? -> 이 때 B팀이 얻는 승점 
-        
-        //차례로  순서보다 큰걸 집어넣는데 최대한 차이 안나는걸로 집어넣는다.
-        Arrays.sort(B);
         Arrays.sort(A);
-        List<Integer> arr = new ArrayList<>();
-        for (int i=0; i<B.length; i++){
-            arr.add(B[i]);
-        }
+        Arrays.sort(B);
+        int idx = 0;
         
-        Collections.sort(arr);
-        //5 1 3 7
+        // 1 3 5 7 
+        // 2 2 6 8
+
+//         for(int i = 0; i < A.length; i++){
+//             if(idx >= A.length) break;
+
+//             while(B[idx] <= A[i]){
+//                 idx++;
+//                 if(idx >= A.length) break;
+//             }
+
+//             if(idx >= A.length) break;
+//             idx++;
+//             answer++;
+
+//         }
         
-        // 28
+
         for (int i=0; i<A.length; i++){
-            int num = A[i];
-            
-            for (int j=0; j<arr.size(); j++){
-                if(arr.get(j)>A[i]){
-                    arr.remove(j);
+            for (int j=idx; j<B.length; j++){
+                if(B[j]>A[i]){
                     answer++;
-                    break;
+                    idx = j+1;
+                    break; 
                 }
             }
         }
-        
+
         return answer;
     }
 }
