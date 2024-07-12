eff2f8b6856f1e1fbd3dee78ff9c9df4276e3a84
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-07-13 03:04:44 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.84 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,73 +1,89 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
 class Solution {
-    
-  static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static char[][] map;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
     static int R,C;
-    static Queue<int[]> queue = new LinkedList<>();
+    static char[][] map;
     static boolean[][] visited;
-    static int answer = Integer.MAX_VALUE;
+    static Queue<int[]> queue = new LinkedList<>();
     
     public int solution(String[] board) {
-
-        map = new char[board.length][board[0].length()];
-        visited = new boolean[board.length][board[0].length()];
-        R = board.length;;
+        int answer = 0;
+        R= board.length;
         C = board[0].length();
-        for (int i=0; i< board.length; i++){
-            String tmp = board[i];
-            for (int j=0; j<tmp.length(); j++){
-                map[i][j] = tmp.charAt(j);
+        map = new char[R][C];
+        visited = new boolean[R][C];
+        // D가 닿을 때 까지 이동.  
+        // 최소 움직임.
+        int i=0; 
+        for (String str:board){
+            for (int j=0; j<C; j++){
+                map[i][j]=str.charAt(j);
                 if(map[i][j]=='R'){
-                    queue.offer(new int[]{i, j, 0});
+                    queue.offer(new int[]{i,j,0});
                     visited[i][j]=true;
-                }
+                } 
             }
+            i++;
         }
-
-        BFS();
-        if(answer ==Integer.MAX_VALUE) return -1;
-        else return answer;
         
+        answer = BFS();
+        if(answer==Integer.MAX_VALUE) return -1;
+        else return answer;
     }
-
-    public static void BFS(){
+    
+    public static int BFS(){
+        int answer = Integer.MAX_VALUE;
         while(!queue.isEmpty()){
             int[] tmp = queue.poll();
             int x = tmp[0];
             int y = tmp[1];
-            int count = tmp[2];
-
+            int cnt = tmp[2];
+            
             if(map[x][y]=='G'){
-                answer = Math.min(answer,count);
-        
-            }
+                answer = Math.min(answer,cnt);
+            } 
+            
             for (int i=0; i<4; i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-
-                if(isRange(nx,ny) &&  map[nx][ny]!='D') {
-                    while (isRange(nx, ny) && map[nx][ny] != 'D' ) { // 다음 좌표가 D가 아니고 범위 안이면 반복
-                        nx += dx[i];
-                        ny += dy[i];
+                int nx =x+dx[i];
+                int ny =y+dy[i];
+                if(!isRange(nx,ny) || map[nx][ny]=='D') continue;
+                // visited를 공유하는게 문제다.
+                
+                    int[] points = slideMove(nx,ny,i);
+                    int px=points[0];
+                    int py=points[1];
+                    
+                    if(!visited[px][py]){
+                        visited[px][py]=true;
+                        queue.offer(new int[]{px,py,cnt+1});    
                     }
-
-                    nx-=dx[i];
-                    ny-=dy[i];
-                    if(!visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny, count + 1});
-                    }
-                }
-                // 위 반복을 탈출했으면 다음
+    
+                
             }
         }
+        
+        return answer;
     }
-
+    
+    public static int[] slideMove(int x, int y, int dir){
+        while(true){
+            
+            int nx = x+dx[dir];
+            int ny = y+dy[dir];
+            
+            //다음칸이 범위를 벗어나거나 혹은 D이면 현재 좌표를 리턴한다.
+            if(!isRange(nx,ny) || map[nx][ny]=='D'){
+                return new int[]{x,y};
+            }
+           
+            x =nx;
+            y =ny;
+        }
+    }
+    
     public static boolean isRange(int nx, int ny){
-        if(nx>=0 && nx<R && ny>=0 && ny<C) return true;
-        return false;
+        return (nx>=0 && nx<R && ny>=0 && ny<C);
     }
 }
