01140d5f012fb352a0d478634593ba973de88cd5
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-07-31 08:59:10 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,48 +1,43 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] dp ;
+    static int[][] cost ;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][N];
-        int[][] dp = new int[N][3]; // i번째 집까지 0(r) 1(g) 2(b)로 칠했을 때의 최소비용
-
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N][3];
+        cost = new int[N][3];
 
         for (int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j=0; j<3; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //초기화
-        for (int i=0; i<N; i++){
-            for (int j=0; j<3; j++){
-                dp[i][j]=10000001;
-            }
+        for (int[] arr:dp){
+            Arrays.fill(arr, Integer.MAX_VALUE);
         }
 
+        //초기화.
+        dp[0][0]=cost[0][0];
+        dp[0][1]=cost[0][1];
+        dp[0][2]=cost[0][2];
 
-
-        //인접한 칸과 같은 색 X
-
-        dp[0][0] = arr[0][0];
-        dp[0][1] = arr[0][1];
-        dp[0][2] = arr[0][2];
-
-        for (int j=1; j<N; j++){
-            //j번째 집을 0(r)으로 칠했을 때 최소비용
-            dp[j][0] = Math.min(dp[j - 1][1] + arr[j][0], dp[j - 1][2] + arr[j][0]);
-            dp[j][1] = Math.min(dp[j - 1][0] + arr[j][1], dp[j - 1][2] + arr[j][1]);
-            dp[j][2] = Math.min(dp[j - 1][0] + arr[j][2], dp[j - 1][1] + arr[j][2]);
+        for (int i=1; i<N; i++){
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2])+cost[i][0];
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2])+cost[i][1];
+            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1])+cost[i][2];
         }
 
-        System.out.println(Math.min(dp[N-1][2],Math.min(dp[N-1][0],dp[N-1][1])));
-
-
+        int answer = Math.min(dp[N-1][0],Math.min(dp[N-1][1],dp[N-1][2]));
+        System.out.println(answer);
     }
+
 }
