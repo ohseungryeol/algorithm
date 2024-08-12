5a4e6c95889477cf01caa4b8a1929264937950ea
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-08-12 16:41:28 +0900

    [level 2] Title: 디펜스 게임, Time: 148.62 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,41 +1,38 @@
 
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-
+import java.util.*;
 class Solution {
+
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
+
+        // 최대한 많은 라운드를 진행
+        // 순서는 보장해야함
+
+        // 4 2 4 5 3 3 1
+        // 무적권으로 많은 애들을 쳐내는 것이 중요
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        int round =0;
-        int remain=n;
-
+        
+        // n=5 k=1
         for (int i=0; i<enemy.length; i++){
-
-            //지나온 라운드에서 가장 큰 값만 무적권을 쓰자
-            remain -= enemy[i];
             pq.offer(enemy[i]);
-            if(remain<0){
+            n-=enemy[i];
+            
+            if(n<0){
                 if(k>0){
+                    n+=pq.poll();
                     k--;
-                    remain+=pq.poll();
-                    round++;
+                    answer++;
+                    continue;
                 } else {
                     break;
-                }
-            } else{
-                round++;
+                } 
             }
-
-
+            answer++;
         }
-
-        System.out.println(round);
-
-        return round;
+        return answer;
     }
 
    
 }
+
+
