4d6382e46b22cd199f19ecd10c3c7bb46639419c
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-08-16 14:25:34 +0900

    [level 2] Title: 스킬트리, Time: 0.20 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 class Solution {
@@ -10,36 +9,36 @@ class Solution {
             char c = skill.charAt(i);
             map.put(c,seq++);
         }
-
+        
         for(String str:skill_trees){
             if(impossibleSkill(str)){
                 answer++;
             }
-
+            
         }
-
+        
         return answer;
     }
-
-    public static boolean impossibleSkill(String str){
-        
     
-        // 2는 1이 앞, 3은 2가 앞, 4는 3이 앞 
-        int seq=0;
+    public static boolean impossibleSkill(String str){
+        System.out.println("Str===="+str);
+        int last=26;
         for(int i=0; i<str.length(); i++){
-            char c = str.charAt(i);
-            
-            if(map.containsKey(c)){
-                if(map.get(c)!=seq+1) return false;
-                seq=map.get(c);
+                char c =str.charAt(i);
+            System.out.println(map.get(c));
+                if(map.containsKey(c)){
+                    //CBD  123
+                    if(map.get(c)<last){
+                        return false;
+                    } else{
+                        last = map.get(c); //2     
+                    }
+                    
+                //BACDE
+                //2 13
+                }
             }
-        }
-
+        
         return true;
-    }
-
-    public static void main(String[] args) {
-        Solution main = new Solution();
-        main.solution("CBD",new String[]{"BACDE", "CBADF", "AECB", "BDA"});
     }
 }
