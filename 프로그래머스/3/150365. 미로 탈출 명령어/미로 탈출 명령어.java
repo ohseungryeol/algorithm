7ef1f5bd787e928b491cf5986d1d7f99778d8a3d
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2024-08-21 00:51:50 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 20.59 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,62 +1,74 @@
-public class Solution {
+import java.util.*;
+
+class Solution {
+    static int R;
+    static int C;
+    static boolean flag;
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, -1, 1, 0};
+    static String answer = "";
+    static int sx,sy;
+    static int ex,ey;
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        StringBuilder answer = new StringBuilder();
-        int[] dlru_num = new int[4]; // d, l, r, u 순서로 저장
-        
-        int dx = r - x;
-        int dy = c - y;
-        int distance = Math.abs(dx) + Math.abs(dy);
-        
-        if (k < distance || (k - distance) % 2 != 0) {
+        R=n;
+        C=m;
+        sx=x-1;
+        sy=y-1;
+        ex=r-1;
+        ey=c-1;
+
+        int distance = Math.abs(x-r)+Math.abs((y-c));
+
+        if(k<distance || (k-distance)%2!=0){
             return "impossible";
         }
 
-        // 방향별 이동량 계산
-        dlru_num[0] = (dx >= 0) ? dx : 0; // d
-        dlru_num[1] = (dy < 0) ? -dy : 0; // l
-        dlru_num[2] = (dy >= 0) ? dy : 0; // r
-        dlru_num[3] = (dx < 0) ? -dx : 0; // u
-
-        k -= distance;
-        int max_d = k / 2;
-
-        int height = n - Math.max(x, r);
-        int left = Math.min(y, c);
-
-        while (max_d > 0 && height > 0) {
-            dlru_num[0]++; // d
-            dlru_num[3]++; // u
-            height--;
-            max_d--;
-        }
-
-        while (max_d > 0 && left > 1) {
-            dlru_num[1]++; // l
-            dlru_num[2]++; // r
-            left--;
-            max_d--;
-        }
-
-        // 문자열 생성
-        for (int i = 0; i < 4; i++) {
-            char move = ' ';
-            switch(i) {
-                case 0: move = 'd'; break;
-                case 1: move = 'l'; break;
-                case 2: move = 'r'; break;
-                case 3: move = 'u'; break;
-            }
-
-            if (i == 2 && max_d > 0) {
-                for (int j = 0; j < max_d; j++) {
-                    answer.append("rl");
-                }
-            }
-            for (int j = 0; j < dlru_num[i]; j++) {
-                answer.append(move);
-            }
-        }
+        flag = false;
         
-        return answer.toString();
+        DFS(sx,sy,0,k,"");
+
+       
+        //System.out.println(routes.get(0));
+        return answer;
+    }
+
+    public static void DFS(int x,int y,int count,int k,String route){
+        if(count==k){
+            if(x==ex && y==ey){
+                answer = route;
+                flag=true;
+            }
+            return;
+        }
+
+        for (int i=0; i<4; i++){ //l r u d
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            char c;
+
+
+            if(isRange(nx,ny) && (Math.abs(ex-x)+Math.abs(ey-y)<=k-count+1) && !flag){
+                if(i==0){
+                    c ='d';
+                } else if (i==1){
+                    c='l';
+                } else if (i==2){
+                    c ='r';
+                } else {
+                    c = 'u';
+                }
+                DFS(nx,ny,count+1,k,route+c);
+            }
+        }
+    }
+
+    public static boolean isRange(int nx, int ny){
+        return (nx>=0 && nx<R && ny>=0 && ny<C);
+    }
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.solution(	3, 4, 2, 3, 3, 1, 5);
     }
 }
